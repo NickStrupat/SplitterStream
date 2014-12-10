1c131f74f2dfc75fa708c29ae666285c4a8edaf5
Nick Strupat <nick.strupat@gmail.com>
2014-12-10 16:05:11 -0500

    added unit test and fixed .NET 4.0 assembly name

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test {
+	[TestClass]
+	public class UnitTest1 {
+		[TestMethod]
+		public void TestMethod1() {
+			const String text = "This is some text";
+			var bytes = Encoding.Default.GetBytes(text);
+			using (var source = new MemoryStream(bytes))
+			using (var destination1 = new MemoryStream())
+			using (var destination2 = new MemoryStream())
+			using (var splitter = new SplitterStream.SplitterStream(destination1, destination2))
+			using (var destination1Reader = new StreamReader(destination1))
+			using (var destination2Reader = new StreamReader(destination2)) {
+				source.CopyTo(splitter);
+				destination1.Position = 0;
+				var text1 = destination1Reader.ReadToEnd();
+				Assert.AreEqual(text, text1);
+				destination2.Position = 0;
+				var text2 = destination2Reader.ReadToEnd();
+				Assert.AreEqual(text, text2);
+			}
+		}
+	}
+}
